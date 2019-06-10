9f025206fc8e74ce281d6b7203f3ad30298c54b8
Frank Valencia <frankva21@yahoo.co.uk>
2019-06-10 12:38:19 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,26 @@
+learn
+
 var index = require("../index.js")
 
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+describe('what is a test', () => {
+  describe('name', () => {
+    it('returns "susan"', () => {
+     expect(index.name).toEqual('susan')
     })
   })
 
 
-  describe('Height', () => {
-    it('is less than 40', () => {
+ describe('height', () => {
+  it('is greater than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
-  })
+ })
 
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
+describe('Message', () => {
+it('gives the name and height', () => {
+       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
   })
-
-})
-
+  
+}) 
